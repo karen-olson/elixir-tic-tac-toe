9acaff77ad8d6f13baae8d33d3358346869c60cf
Karen Olson <karmarie@gmail.com>
2022-07-07 14:35:19 -0500

    Create walking skeleton

--- lib/elixir_tic_tac_toe_basic.ex
+++ lib/elixir_tic_tac_toe_basic.ex
@@ -1,8 +1,11 @@
 defmodule ElixirTicTacToeBasic do
+  @moduledoc """
+  Basic Tic Tac Toe Game
+  """
+
   def welcome do
     IO.puts("Welcome to Tic Tac Toe!")
   end
-
 end
 
 ElixirTicTacToeBasic.welcome()
